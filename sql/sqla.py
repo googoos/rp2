5a605737fb1df4e2d2617c03d6246a88fd8d0e36
Gugus Sakti <Gugus Sakti>
2016-05-09 18:48:46 +0700

    Book 2 Chapter 6: Creating Table

---
+++ sql/sqla.py
@@ -0,0 +1,17 @@
+# Create an SQLite3 database and table
+
+# import the SQLlite3 library
+import sqlite3
+
+# create a new  database if the database doesn't already exist
+conn = sqlite3.connect("new.db")
+
+# get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+# create table
+cursor.execute("""CREATE TABLE population
+				(city TEXT, state TEXT, population INT)
+				""")
+# close the data base connection
+conn.close()
