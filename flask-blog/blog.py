875440a480f95d1a75119ae4991849a664594d9f
Gugus A. Sakti <gugussakti@gmail.com>
2016-05-14 19:22:58 +0700

    flask blog: logout

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -1,37 +1,54 @@
+"""blog.py - controler."""
+
+
+# import
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 import sqlite3
-import os
 
 # configuration
 DATABASE = 'blog.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
-SECRETKEY = b'\xd5r\xb1\xd6\x14\x03\xca\xb1\x1cn\xad\t\xdcj\x86:P\xdfGf\x1b\xac\r!'
+SECRETKEY = 'd76s1b762IM6usNMFk4qF5785k03325K'
 
 app = Flask(__name__)
 
+# pulls in configuration by looking for UPPERCASE variables
 app.config.from_object(__name__)
 
+
 def connect_db():
+    """Funtion used for connecting to the database."""
     return sqlite3.connect(app.config['DATABASE'])
+
 
 @app.route('/')
 def login():
+    """Login page function."""
     error = None
-    if request.method == 'POST'
-        if request.form['username'] != app.config.['USERNAME'] or \
-        request.form['password'] != app.config.['PASSWORD']:
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+                request.form['password'] != app.config['PASSWORD']:
             error = 'Invalid Credentials. Please try again.'
         else:
             session['logged_in'] = True
             return redirect(url_for('main'))
     return render_template('login.html', error=error)
 
+
 @app.route('/main')
 def main():
+    """Main page function."""
     return render_template('main.html')
 
+
+@app.route('/logout')
+def logout():
+    """Logout page fuction."""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
