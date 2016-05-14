005f34472f1edb7c41a16cd2a2bd26979991cf8e
Gugus Sakti <Gugus Sakti>
2016-05-14 15:44:24 +0700

    flask bog app: user login

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -1,31 +1,33 @@
-"""flask blog app."""
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 import sqlite3
+import os
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORDS = 'admin'
+SECRETKEY = b'\xd5r\xb1\xd6\x14\x03\xca\xb1\x1cn\xad\t\xdcj\x86:P\xdfGf\x1b\xac\r!'
 
 app = Flask(__name__)
 
 app.config.from_object(__name__)
 
-
 def connect_db():
-    """connecting to database."""
     return sqlite3.connect(app.config['DATABASE'])
 
-
 @app.route('/')
-def main():
-    """main page."""
-    return render_template('main.html')
-
-
-@app.route('/login')
 def login():
-    """login page."""
     return render_template('login.html')
 
-if __name__ == '__name__':
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+@app.route('/sk')
+def skey():
+    return str(os.urandom(24))
+
+
+if __name__ == '__main__':
     app.run(debug=True)
